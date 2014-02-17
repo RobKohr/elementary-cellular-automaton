0e946231785622b872b15a329a5cf5f4b362d6e7
Robert Kohr <Robert.Kohr-contractor@adp.com>
2014-02-16 19:00:50 -0500

    calculate data and paint to canvas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   app.init();
-  var buttons = ['start', 'stop', 'reset'];
+  var buttons = ['start', 'stop', 'reset', 'step'];
   for(var i=0; i<buttons.length; i++){
     var button = buttons[i];
     $('#'+button).click(app[button]);
@@ -10,26 +10,87 @@ $(document).ready(function(){
 
 var app = {}
 app.init = function(){
-  app.reset();
+  app.iteration_dom = $('#iteration');//so we don't have too look it up each time
   draw.init();
+  app.reset();
 };
 
+app.calc = function(left, center, right){
+  var key = '';
+  key += left ? '1' :'0';
+  key += center ? '1' :'0';
+  key += right ? '1' :'0';
+  var mapping = {
+    '111':false,
+    '110':false,
+    '101':false,
+    '100':true,
+    '011':true,
+    '010':true,
+    '001':true,   
+    '000':false
+  }
+  return mapping[key];
+}
+
 app.step = function(){
-  
+  //the number columns left and right of middle = rows
+  var rows = app.data.length;
+  app.iteration_dom.html(rows+1);
+  //previous row
+  var prev = app.data[app.data.length-1];
+  //new current row
+  cur = {};
+  //set current row in data obj
+  app.data[app.data.length] = cur;
+  for(var c = -rows; c<=rows; c++){
+    cur[c] = app.calc(prev[c-1], prev[c], prev[c+1]);
+  }
+  //  app.console(); // see data in console
+  draw.data(app.data); // see data drawn on canvas
+}
+
+app.console = function(){
+  var rows = app.data.length;
+  console.log("=============================");
+  console.log("Rows = "+rows+"   ==============");
+  console.log("=============================");
+  for(var r = 0; r < rows; r++){
+    var row_str = '';
+    for(var c = -rows; c<=rows; c++){
+      if(app.data[r][c]){
+        row_str += '1';
+      }else{
+        row_str += '0';
+      }
+    }
+    console.log(row_str);
+  }
 }
 
 app.start = function(){
-
+  if(app.interval_id){
+    console.log('Ops, seems we already started... don\'t want to do it twice');
+    return;
+  }
+  var interval = 1000;
+  app.interval_id = setInterval(app.step, interval);
 }
 
 app.stop = function(){
-
+  if(!app.interval_id){
+    return;//do nothing
+  }
+  clearInterval(app.interval_id);
+  app.interval_id = null;
 }
 
 app.reset = function(){
+  app.stop();
+  app.iteration_dom.html(0);
   app.interval_id = null;
   // each index in the array is a row that contains an object indexing true cells
   // in the object 0 is the center of the screen, and indexes can be negative an positive
   app.data = [{0:true}]; 
-  app.row_count = 1;
+  draw.clear();
 }
--- js/draw.js
+++ js/draw.js
@@ -2,12 +2,55 @@ var draw = {};
 draw.init = function(){
   draw.fullscreen();
   $(window).resize(draw.fullscreen);
+  var ctx = document.getElementById('canvas').getContext('2d');
+  ctx.strokeStyle = "blue";
+  draw.ctx = ctx;
+  
 };
+
+draw.clear = function(){
+  draw.ctx.clearRect(0,0,draw.width, draw.height);
+}
+
 
 //have the canvas take over the screen
 draw.fullscreen = function(){
-  var width = $(window).width();
-  var height = $(window).height();
+  var width = draw.width = $(window).width();
+  var height = draw.height = $(window).height();
+  draw.min_dimension = draw.width; //used to calc square size
+  if(draw.height < draw.width){
+    draw.min_dimension = draw.height;
+  }
   $('#canvas').attr('width', width);
   $('#canvas').attr('height', height);
+}
+
+draw.data = function(data){
+  draw.clear();
+  var columns = data.length*2 + 1;
+  var rows = data.length;
+  var square_width = (draw.width)/columns;
+  var square_height = (draw.height)/rows;
+  //square size should be the min of the two
+  var square_size = square_width < square_height ? square_width : square_height;
+  var rows = data.length;
+  var start_top = draw.height;
+  var start_left = draw.width;
+  start_top = start_left = 0;
+  for(var r = 0; r < rows; r++){
+    var row_str = '';
+    for(var c = -rows; c<=rows; c++){
+      //make it so lowest negative number is x=0
+      var c_fix = c+rows;
+      var x = start_left + (c_fix * square_size);
+      var y = start_top + (r * square_size);
+      if(app.data[r][c]){
+        draw.ctx.fillRect (x, y, square_size-1, square_size-1);
+      }      
+    }
+  }
+
+
+
+
 }
